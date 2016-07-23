e18cacafeac0f2267b9199c593ce8a9ab151bee0
10Dozen <steel.mjr@gmail.com>
2016-07-24 01:38:58 +0300

    Update config.cpp

--- dzn_Action_Button_Mod_Expand/config.cpp
+++ dzn_Action_Button_Mod_Expand/config.cpp
@@ -2,6 +2,8 @@ class CfgPatches
 {
 	class dzn_Action_Button_Mod_Expand
 	{		
+		units[] = {};
+		weapons[] = {};
 		requiredVersion = 1.0;
 		requiredAddons[] = {"CBA_MAIN", "Action_Button_Mod"};
 		author[] = {"10Dozen"};
